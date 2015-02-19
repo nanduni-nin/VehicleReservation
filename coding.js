0ac71954da807574f5e1f566f953a8f7443277c5
Nanduni Indeewaree Nimalsiri <nanduni.12@cse.mrt.ac.lk>
2015-02-19 05:13:06 -0800

    first commit

---
+++ coding.js
@@ -0,0 +1,7 @@
+$("#container_image").PictureCut({
+                  InputOfImageDirectory       : "image",
+                  PluginFolderOnServer        : "/jquery.picture.cut/",
+                  FolderOnServer              : "/uploads/",
+                  EnableCrop                  : true,
+                  CropWindowStyle             : "Bootstrap"
+              });
---
+++ function.js
@@ -0,0 +1,11 @@
+function showHide()
+        {
+            if(document.getElementById('Checkbox1').checked)
+            {
+                document.getElementById('delivery').style.display = 'block';
+            }
+            else
+            {
+                document.getElementById('delivery').style.display = 'none';
+            }
+        }
